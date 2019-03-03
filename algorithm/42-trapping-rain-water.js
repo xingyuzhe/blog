aabdc39404c1b8f3a0a6463d2e1c65b82f3c1249
yuzhe <=>
2019-03-04 03:14:45 +0800

    add leetcode solutions

--- algorithm/42-trapping-rain-water.js
+++ algorithm/42-trapping-rain-water.js
@@ -24,6 +24,54 @@ var trap1 = function(height) {
     return ans
 };
 
-var trap = function(right) {
+/**
+ * 动态规划
+ * @param {*} height 
+ */
+var trap2 = function(height) {
+  if (!height || !height.length) {
+    return 0
+  }
 
+  const leftMax = []
+  const rightMax = []
+  const l = height.length
+
+  leftMax[0] = height[0]
+
+  for (let i = 1; i < l; i++) {
+    leftMax[i] = Math.max(height[i], leftMax[i - 1])
+  }
+
+  rightMax[l - 1] = height[l - 1]
+
+  for (let i = l - 2; i >= 0; i--) {
+    rightMax[i] = Math.max(height[i], rightMax[i + 1])
+  }
+
+  let ans = 0
+
+  for (let i = 1; i < l - 1; i++) {
+    ans += Math.min(leftMax[i], rightMax[i]) - height[i]
+  }
+
+  return ans
 }
+
+/**
+ * 使用stack
+ * @param {*} height 
+ */
+var trap3 = function(height) {
+  // @TODO
+}
+
+
+/**
+ * 双指针
+ * @param {*} height 
+ */
+var trap4 = function(height) {
+  // @TODO
+}
+
