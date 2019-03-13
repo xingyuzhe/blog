928264472adddffdc6940e315ee96a89e7e5eb40
yuzhe <=>
2019-03-14 03:36:24 +0800

    chore: add leetcode solutions

--- algorithm/136-single-number.js
+++ algorithm/136-single-number.js
@@ -1,1 +1,3 @@
-//@TODO
+var singleNumber = function(nums) {
+    return nums.reduce((prev, next) => prev ^ next, 0)
+};
--- algorithm/142-linked-list-cycle-ii.js
+++ algorithm/142-linked-list-cycle-ii.js
@@ -1,1 +1,37 @@
-// @TODO
+var detectCycle1 = function (head) {
+  if (!head) return null
+
+  const nodeMap = new Map()
+
+  let current = head
+
+  while(current && !(nodeMap.has(current))) {
+    nodeMap.set(current, 1)
+    current = current.next
+  }
+
+  return nodeMap.get(current) ? current : null
+};
+
+var detectCycle = function(head) {
+  if (!head) return null
+
+  let fast = head
+  let slow = head
+
+  while(slow && fast && fast.next) {
+    slow = slow.next
+    fast = fast.next.next
+
+    if (slow === fast) {
+      while(head !== slow) {
+        head = head.next
+        slow = slow.next
+      }
+
+      return head
+    }
+  }
+
+  return null
+}
