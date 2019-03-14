e147390e3f0982ec396e1d9b6cf720ccfd0d93f8
yuzhe <=>
2019-03-15 04:40:52 +0800

    chore: add leetcode solutions

--- algorithm/237-delete-node-in-a-linked-list.js
+++ algorithm/237-delete-node-in-a-linked-list.js
@@ -1,14 +1,4 @@
-/**
- * Definition for singly-linked list.
- * function ListNode(val) {
- *     this.val = val;
- *     this.next = null;
- * }
- */
-/**
- * @param {ListNode} node
- * @return {void} Do not return anything, modify node in-place instead.
- */
 var deleteNode = function(node) {
-    // @TODO
+  node.val = node.next.val
+  node.next = node.next.next
 };
--- algorithm/sorting/merge-sort.js
+++ algorithm/sorting/merge-sort.js
@@ -1,7 +1,7 @@
 class Solution {
   static mergeSort(nums) {
     if (!nums || nums.length <= 1) return nums
-    const clone = nums.slice()
+    const clone = Array(nums.length)
     this.recursive(nums, clone, 0, nums.length - 1)
   
     return nums
@@ -10,11 +10,8 @@ class Solution {
   static recursive(origin, clone, start, end) {
     if (start >= end) return
 
-    console.log(start, end)
-
-    const middle = Math.ceil(start + ((end - start) / 2))
-
-    console.log(middle)
+    // 如果是ceil, 这里出现0, 1的情况时就会无限循环
+    const middle = Math.floor((start + end) / 2)
 
     this.recursive(origin, clone, start, middle)
     this.recursive(origin, clone, middle + 1, end)
@@ -25,12 +22,17 @@ class Solution {
   static merge(origin, clone, start, middle, end) {
     let i = start, j = middle + 1
 
+    // 每次都要把上次排过的copy到临时数组中
+    for (let x = start; x <= end; x++) {
+      clone[x] = origin[x]
+    }
+
     for (let k = start; k <= end; k++) {
       if (i > middle) {
         origin[k] = clone[j++]
       } else if (j > end) {
         origin[k] = clone[i++]
-      } else if (clone[i] > clone[j]) {
+      } else if (clone[i] < clone[j]) { // 把小的排前面
         origin[k] = clone[i++]
       } else {
         origin[k] = clone[j++]
